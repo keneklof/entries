b4ac94566f8d7a505212e66fdd38eb7a0042d90b
keneklof <keni.eklof@gmail.com>
2026-02-13 12:24:19 +0200

    Update 13.2.2026 12:24

---
+++ pilots/entries.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https: //kit.fontawesome.com/9e7a1653cf.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container py-5">
+      <h1 class="text-center">Ilmoittautuneet</h1>
+      <p class="lead text-center">Tähän tulee ilmoittutuneiden lista</p>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
